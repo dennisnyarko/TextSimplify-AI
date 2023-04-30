ee34fd597557fe5a1e3c3565f16f5d4f95a0ab95
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-04-30 23:39:52 +0000

    show last copied url

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,14 +9,14 @@ const Demo = () => {
         summary: '',
     });
 
-    const [ allArticles, setAllArticles ] = useState([]);
+    const [allArticles, setAllArticles] = useState([]);
 
     const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
     useEffect(() => {
         const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'))
 
-        if(articlesFromLocalStorage) {
+        if (articlesFromLocalStorage) {
             setAllArticles(articlesFromLocalStorage)
         }
 
@@ -24,7 +24,7 @@ const Demo = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
+
         const { data } = await getSummary({ articleUrl: article.url });
 
         if (data?.summary) {
@@ -39,26 +39,49 @@ const Demo = () => {
     }
 
 
-  return (
-    <section className="mt-16 w-full max-w-xl">
-    {/* Search */}
-    <div className="flex flex-col w-full gap-2">
-    <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
-    <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5"/>
-    <input type="url" placeholder="Enter a URL" value={article.url} onChange={(e) => setArticle({ ... article, url: e.target.value})} required className="url_input peer"/>
-    <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">↵
+    return (
+        <section className="mt-16 w-full max-w-xl">
+            {/* Search */}
+            <div className="flex flex-col w-full gap-2">
+                <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
+                    <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5" />
+                    <input type="url" placeholder="Enter a URL" value={article.url} onChange={(e) => setArticle({ ...article, url: e.target.value })} required className="url_input peer" />
+                    <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">↵
 
-    </button>
+                    </button>
 
-    </form>
+                </form>
 
-    {/* Browse URL History */}
+                {/* Browse URL History */}
+                <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+                    {allArticles.map((item, index) => (
+                        <div
+                            key={`link-${index}`}
+                            onClick={() => setArticle(item)}
+                            className="link_card"
+                        >
 
-    </div>
+                            <div className="copy_btn">
+                                <img
+                                    src={copy}
+                                    alt="copy_icon"
+                                    className="w-[40%] h-[40%] object-contain"
+                                />
+                            </div>
+                            <p>
+                                {item.url}
+                            </p>
 
-    {/* Display Results */}
-    </section>
-  )
+                        </div>
+                    ))}
+
+                </div>
+
+            </div>
+
+            {/* Display Results */}
+        </section>
+    )
 }
 
 export default Demo
